fd10ffd909963d06538fc0ccdbb6b90d98792088
wardk6907 <43149309+wardk6907@users.noreply.github.com>
2018-09-10 14:13:22 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_KaylaWard.py
@@ -0,0 +1,14 @@
+# Sales Prediction
+# 10 Sept 2018
+# CTI-110 P2T1 - Sales Prediction
+# Kayla Ward
+#
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
